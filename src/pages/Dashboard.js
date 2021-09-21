d698e40199af9a74ded90689f753dab5435529d8
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-09-21 22:26:48 +0300

    Dashboard created

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CardComp from '../components/CardComp'
+
+const Dashboard = () => {
+    return (
+        <div>
+            <CardComp/>
+        </div>
+    )
+}
+
+export default Dashboard
